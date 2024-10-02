56ca7a326b9e99d90abecfec4c9dfd4fa7b57d22
IFCE-Lab4 <guilherme.pereira10@aluno.ifce.edu.br>
2024-10-02 13:24:02 -0300

    André Vinicíus Lima Ferreira; Guilherme Pereira Ferreira; Luiz Everton Oliveira da Silva

---
+++ app/src/main/java/com/example/cedrinhoverde/MainActivity4.java
@@ -0,0 +1,14 @@
+package com.example.cedrinhoverde;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tela_pesquisar);
+    }
+}
---
+++ app/src/main/java/com/example/cedrinhoverde/MainActivity6.java
@@ -0,0 +1,14 @@
+package com.example.cedrinhoverde;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity6 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tela_para_voce);
+    }
+}
